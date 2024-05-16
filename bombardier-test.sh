c88efa9ae6a6183e43dd553751394b7185e4eabb
Ilya Pilipchuk <ilja.pilipchuks@gmail.com>
2024-05-16 15:55:37 +0200

    new_changes

--- bombardier-test.sh
+++ bombardier-test.sh
@@ -10,10 +10,10 @@ SRVADD=$1
 docker pull alpine/bombardier:latest
 
 # Bombarding our service for 10s using 250 connections
-docker run -ti --rm alpine/bombardier -c 2 -d 10s -l $SRVADD >> container_250_01.txt
+docker run -ti --rm alpine/bombardier -c 250 -d 10s -l $SRVADD >> container_250_01.txt
 
 # Bombarding our service for 30s using 500 connections
-#docker run -ti --rm alpine/bombardier -c 500 -d 30s -l $SRVADD >> container_500_02.txt
+docker run -ti --rm alpine/bombardier -c 500 -d 30s -l $SRVADD >> container_500_02.txt
 
 # Bombarding our service for 30s using 1000 connections
-#docker run -ti --rm alpine/bombardier -c 1000 -d 60s -l $SRVADD >> container_1000_03.txt
+docker run -ti --rm alpine/bombardier -c 1000 -d 60s -l $SRVADD >> container_1000_03.txt
