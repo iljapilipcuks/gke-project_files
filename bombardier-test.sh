17478b2890b0440def4d0c2e14b26040701937e1
Ilya Pilipchuk <ilja.pilipchuks@gmail.com>
2024-05-27 13:32:26 +0200

    Last commit

--- bombardier-test.sh
+++ bombardier-test.sh
@@ -3,7 +3,6 @@
 # Packages installed for testing:
 # sudo apt-get update
 # sudo apt-get install -y bombardier
-# sudo apt-get install -y default-mysql-client
 # sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # $1-ip address of tested application
@@ -13,10 +12,10 @@ SRVADD=$1
 docker pull alpine/bombardier:latest
 
 # Bombarding our service for 10s using 2500 requests
-docker run -ti --rm alpine/bombardier -c 2500 -d 60s -l $SRVADD >> bombardier_2500.txt
+docker run -ti --rm alpine/bombardier -c 10 -n 2500 -d 60s -l $SRVADD >> bombardier_2500.txt
 
 # Bombarding our service for 30s using 5000 requests
-docker run -ti --rm alpine/bombardier -c 5000 -d 60s -l $SRVADD >> bombardier_5000.txt
+docker run -ti --rm alpine/bombardier -c 10 -n 5000 -d 120s -l $SRVADD >> bombardier_5000.txt
 
 # Bombarding our service for 30s using 10000 requests
-docker run -ti --rm alpine/bombardier -c 10000 -d 60s -l $SRVADD >> bombardier_10000.txt
+docker run -ti --rm alpine/bombardier -c 10 -n 10000 -d 300s -l $SRVADD >> bombardier_10000.txt
--- sysbench-test.sh
+++ sysbench-test.sh
@@ -2,20 +2,20 @@
 
 # Packages installed for testing:
 # sudo apt-get update
-# sudo apt-get install -y bombardier
 # sudo apt-get install -y default-mysql-client
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# curl -s https://packagecloud.io/install/repositories/akopytov/sysbench/script.deb.sh | sudo bash
+# sudo apt -y install sysbench
 
 # $1-ip address of tested application database
 DBADD=$1
 
 sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --db-driver=mysql --tables=10 --table-size=2500 prepare
 
-sysbench oltp_read_only --tables=20 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_read_only.txt
+sysbench oltp_read_only --tables=10 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_read_only.txt
 
-sysbench oltp_write_only --tables=20 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_write_only.txt
+sysbench oltp_write_only --tables=10 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_write_only.txt
 
-sysbench oltp_read_write --tables=20 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_read_write.txt
+sysbench oltp_read_write --tables=10 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_read_write.txt
 
-sysbench oltp_delete --tables=20 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_delete.txt
+sysbench oltp_delete --tables=10 --table_size=2500 --mysql-host=$DBADD --mysql-port=3306 --mysql-user=root --mysql-password='mysql_password' --mysql-db=mysql --time=300 --threads=4 --report-interval=15 run >> oltp_delete.txt
 
